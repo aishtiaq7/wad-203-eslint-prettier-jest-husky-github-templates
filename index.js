4dfa4b5685b7076e2425234c2cd8e048d4837564
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-09-19 14:29:35 -0700

    hook test 2

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // index.js
 
-const greeting = "Hello, world!"; // ESLint expects single quotes, but Prettier enforces double quotes
+/**
+ * Adds two numbers together.
+ * @param {number} a - First number.
+ * @param {number} b - Second number.
+ * @returns {number} Sum of a and b.
+ */
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = {
+    add
+}
